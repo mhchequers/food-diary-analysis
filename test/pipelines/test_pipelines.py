fc35a8487f6217815b5721fb7b309d66e3358f18
matthew <matthewchequers@gmail.com>
2021-04-28 21:02:23 -0400

    sorted out testing imports

--- test/pipelines/test_pipelines.py
+++ test/pipelines/test_pipelines.py
@@ -1,3 +1,12 @@
 import pytest 
 
 import pandas as pd
+
+import sys, os
+home_directory = '/home/matthew'
+sys.path.append(home_directory+'/food-diary-analysis/lib')
+print(sys.path)
+from pipelines import pipelines as pipes
+
+def test_pipeline():
+    pass
