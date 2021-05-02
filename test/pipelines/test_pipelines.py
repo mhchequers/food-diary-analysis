00b0bea631a2c35cd0472fbfdd983b35a6330e81
matthew <matthewchequers@gmail.com>
2021-05-02 13:26:54 -0400

    pipelines tests

--- test/pipelines/test_pipelines.py
+++ test/pipelines/test_pipelines.py
@@ -8,5 +8,56 @@ sys.path.append(home_directory+'/food-diary-analysis/lib')
 print(sys.path)
 from pipelines import pipelines as pipes
 
-def test_pipeline():
-    pass
+class DummyComponent1:
+    def __init__(self, df, options={}):
+        self.df = df 
+        self.options = options 
+        self.cols_to_drop = ['test_col_1']
+
+    def run(self):
+        return self.df.drop(self.cols_to_drop, axis=1)
+
+def test_pipeline_with_no_components():
+    input_df = pd.DataFrame(
+        {
+            'test_col_1': [1, 2, 3],
+            'test_col_2': [4, 5, 6]
+        }
+    )
+
+    result_df = pipes.GenericPipeline(input_df, []).run()
+
+    expected_df = pd.DataFrame(
+        {
+            'test_col_1': [1, 2, 3],
+            'test_col_2': [4, 5, 6]
+        }
+    )
+
+    pd.testing.assert_frame_equal(result_df, expected_df)
+
+def test_pipeline_with_one_component():
+    input_df = pd.DataFrame(
+        {
+            'test_col_1': [1, 2, 3],
+            'test_col_2': [4, 5, 6]
+        }
+    )
+
+    result_df = pipes.GenericPipeline(
+        input_df,
+        [
+            (
+                DummyComponent1,
+                {}
+            )
+        ]
+    ).run()
+
+    expected_df = pd.DataFrame(
+        {
+            'test_col_2': [4, 5, 6]
+        }
+    )
+
+    pd.testing.assert_frame_equal(result_df, expected_df)
