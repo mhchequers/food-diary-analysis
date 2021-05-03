6bb0b77710ba976988a2cd84d8f3eb6dae1f2065
matthew <matthewchequers@gmail.com>
2021-05-02 20:20:49 -0400

    abstract a component class

--- lib/pipeline_components/pipeline_components.py
+++ lib/pipeline_components/pipeline_components.py
@@ -1,17 +1,11 @@
 import pandas as pd
 
-class DropColumns:
+class Component:
     def __init__(self, df, options={}):
         self.df = df 
         self.options = options 
-        self.cols_to_drop = self.options.get(
-            "columns_to_drop",
-            []
-        )
-        self._validate_input_columns(self.df.columns.values.tolist())
 
-    def _validate_input_columns(self, df_columns):
-        required_columns = self.cols_to_drop
+    def _validate_input_columns(self, required_columns: list, df_columns: list):
         does_df_have_required_columns = set(required_columns).issubset(set(df_columns))
         if not does_df_have_required_columns:
             raise ValueError(
@@ -19,30 +13,30 @@ class DropColumns:
                     str(required_columns), str(df_columns)
                 )
             )
+
+class DropColumns(Component):
+    def __init__(self, df, options={}):
+        super().__init__(df, options)
+        self.cols_to_drop = self.options.get(
+            "columns_to_drop",
+            []
+        )
+        required_columns = self.cols_to_drop
+        self._validate_input_columns(required_columns, self.df.columns.values.tolist())
     
     def run(self):
         return self.df.drop(self.cols_to_drop, axis=1)
 
-class ConvertDateAndTimeToDatetime:
+class ConvertDateAndTimeToDatetime(Component):
     def __init__(self, df, options={}):
-        self.df = df
-        self._validate_input_columns(self.df.columns.values.tolist())
-        self.options = options
+        super().__init__(df, options)
+        required_columns = ['date', 'time']
+        self._validate_input_columns(required_columns, self.df.columns.values.tolist())
         self.timezone = self.options.get(
             "timezone",
             'US/Eastern'
         )
         self.cols_to_drop = ['date_and_time']
-
-    def _validate_input_columns(self, df_columns):
-        required_columns = ['date', 'time']
-        does_df_have_required_columns = set(required_columns).issubset(set(df_columns))
-        if not does_df_have_required_columns:
-            raise ValueError(
-                'Some or all required columns {} are not found in input dataframe columns {}'.format(
-                    str(required_columns), str(df_columns)
-                )
-            )
 
     def run(self):
         self.df['date_and_time'] = self.df['date'] + ' ' + self.df['time']
